55bbfa8f952df8630f587aacbb67d9fcf69e084b
Matthew <Matthew@Kainalu>
2015-03-02 14:50:11 -0500

    added getters and setters

---
+++ source/MathUtilites.hpp
@@ -0,0 +1,23 @@
+/* 
+ * File:   MathUtilites.hpp
+ * Author: Matthew
+ *
+ * Created on March 2, 2015, 2:45 PM
+ */
+
+#ifndef MATHUTILITES_HPP
+#define	MATHUTILITES_HPP
+
+
+/**
+ * Since MAS is using the standard c++ library as much as possible,
+ * vector and matrix operations are held here. Matrices are represented
+ * in std::vector containers with dimensions folded. 
+ */
+namespace mas{
+    
+}
+
+
+#endif	/* MATHUTILITES_HPP */
+
