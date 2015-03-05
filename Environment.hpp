b336d1f3690b5c05c3c0ba0d984e8adf671c8af8
Matthew <Matthew@Kainalu>
2015-03-05 10:08:55 -0500

    Added Environment class

---
+++ Environment.hpp
@@ -0,0 +1,24 @@
+/* 
+ * File:   Environment.hpp
+ * Author: Matthew
+ *
+ * Created on March 5, 2015, 9:46 AM
+ */
+
+#ifndef ENVIRONMENT_HPP
+#define	ENVIRONMENT_HPP
+
+namespace mas {
+
+    template<typename REAL_T, typename EVAL_T>
+    class Environment : public EvaluationObject<REAL_T, EVAL_T> {
+        
+        public:
+    };
+
+
+}
+
+
+#endif	/* ENVIRONMENT_HPP */
+
